8a273808f2e66426a0f97311e8eab9e7626fd4e3
Joe Varberg <jmvarberg@cmh.edu>
2025-11-13 15:05:53 -0600

    Fix rnaseq folder typo and rework file copy functionality

--- R/create_bcb_report.R
+++ R/create_bcb_report.R
@@ -1,6 +1,7 @@
 #' Generate a standard analysis report from template in the current directory.
 #'
 #' @param file_name Name for the Quarto report file.
+#' @param dir Directory to save the report file to. Default is NULL, which will default to the current working directory.
 #' @param ext_name Which template to use. One of c("bcb-report", "bulk-rnaseq"). Use "bcb-report" for general analyses, and "bulk-rnaseq" to perform DESeq2 analysis of RNAseq datasets.
 #'
 #' @returns Copies template file into working directory.
@@ -10,54 +11,70 @@
 #' if (interactive()) {
 #' create_bcb_report(file_name = "BCB172_Analysis", ext_name = "bcb-report")
 #' }
-create_bcb_report <- function(file_name = NULL, ext_name = c("bcb-report", "bulk-rnaseq")) {
+create_bcb_report <- function(file_name = NULL, dir = NULL, ext_name = c("bcb-report", "bulk-rnaseq")) {
   if (is.null(file_name)) {
     stop("You must provide a valid file_name")
   }
 
+  if (is.null(dir)) {
+    message("Report will be saved in working directory.")
+    dir = getwd()
+  } else if(!dir.exists(dir)) {
+    message(paste0("Creating output directory: ", dir))
+    dir.create(dir, recursive = T)
+  }
+
   # check for available extensions
-  stopifnot("Extension not in package" = ext_name %in% c("bcb-report", "bulk-rnasseq"))
-
-  # check for existing _extensions directory
-  if(!file.exists("_extensions")) dir.create("_extensions")
-  message("Created '_extensions' folder")
-
-  # create folder
-  if(!file.exists(paste0("_extensions/", ext_name))) dir.create(paste0("_extensions/", ext_name))
+  stopifnot("Specified Report Template not found in package. Available options are 'bcb-report' or 'bulk-rnaseq'" = ext_name %in% c("bcb-report", "bulk-rnaseq"))
 
   # copy from internals
-  file.copy(
-    from = system.file(paste0("extdata/_extensions/", ext_name), package = "cmribio"),
-    to = paste0("_extensions/"),
-    overwrite = TRUE,
-    recursive = TRUE,
-    copy.mode = TRUE
-  )
-
-  # logic check to make sure extension files were moved
-  n_files <- length(dir(paste0("_extensions/", ext_name)))
-
-  if(n_files >= 2){
-    message(paste(ext_name, "was installed to _extensions folder in current working directory."))
-  } else {
-    message("Extension appears not to have been created")
-  }
-
   if(ext_name == "bcb-report") {
-    # create new qmd report based on skeleton
-    file.copy("_extensions/bcb-report/cmri-bcb-report-template.qmd",
-              paste0(file_name, ".qmd", collapse = ""))
+
+    # Get the full path to the internal template file from the package
+    template_path <- system.file("extdata/_extensions/bcb-report/cmri-bcb-report-template.qmd",
+                                 package = "cmribio")
+
+    if (template_path == "") {
+      stop("Template file not found in the package.")
+    }
+
+    # Copy the file to the new location with a new name
+    success <- file.copy(
+      from = template_path,
+      to = file.path(dir, paste0(file_name, ".qmd")),
+      overwrite = TRUE
+    )
+
+    if (!success) {
+      stop("File copy failed. Check permissions or paths.")
+    }
   }
+
 
   if(ext_name == "bulk-rnaseq") {
 
-    # create new qmd report based on skeleton
-    file.copy("_extensions/bulk-rnaseq/cmri-bcb-bulk-rnaseq-template.qmd",
-              paste0(file_name, ".qmd", collapse = ""))
+    # Get the full path to the internal template file from the package
+    template_path <- system.file("extdata/_extensions/bulk-rnaseq/cmri-bcb-bulk-rnaseq-template.qmd",
+                                 package = "cmribio")
 
+    if (template_path == "") {
+      stop("Template file not found in the package.")
+    }
+
+    # Copy the file to the new location with a new name
+    success <- file.copy(
+      from = template_path,
+      to = file.path(dir, paste0(file_name, ".qmd")),
+      overwrite = TRUE
+    )
+
+    if (!success) {
+      stop("File copy failed. Check permissions or paths.")
+    }
   }
 
+
   # open the new file in the editor
-  file.edit(paste0(file_name, ".qmd", collapse = ""))
+  file.edit(file.path(dir, paste0(file_name, ".qmd")))
 
 }
